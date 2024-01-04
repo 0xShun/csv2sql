96a17644c4d703f7929f63436bd4c65c65ad5b01
Sean Francis N. Ballais <sean@seanballais.com>
2024-01-05 03:05:46 +0800

    Added survey options that had three possible ratings.

--- csv2sql/evtss_2023/convert.py
+++ csv2sql/evtss_2023/convert.py
@@ -3,6 +3,104 @@ import sqlite3
 
 
 def convert(file: csv.DictReader, db: sqlite3.Cursor):
+    attainments_for_higher_acads: tuple = (
+        'Some university/college without earning a degree',
+        'Associate Degree (A.A., A.S., etc.)',
+        'Bachelor\'s Degree (B.A., B.S., B.Eng, etc.)',
+        'Master\'s Degree (M.A., M.S., M.Eng., MBA, etc.)',
+        'Professional Degree (JD, MD, Ph.D, Ed.D, etc.)',
+    )
+    languages: tuple = (
+        'Ada', 'Apex', 'APL', 'Assembly', 'C', 'C++', 'C#', 'Clojure', 'Cobol',
+        'Crystal', 'Dart', 'DataWeave', 'Delphi', 'Elixir', 'Erlang', 'F#',
+        'Flow', 'Fortran', 'GDScript', 'Go', 'Groovy', 'Haskell', 'HTML/CSS',
+        'Java', 'JavaScript', 'Julia', 'Kotlin', 'LISP', 'Lua', 'MATLAB', 'Nim',
+        'Objective-C', 'OCaml', 'Perl', 'PHP', 'PowerShell', 'Prolog', 'Python',
+        'Ruby', 'R', 'Raku', 'Rust', 'SAS', 'Scala', 'Shell (e.g. Bash, ZSH)',
+        'Solidity', 'SQL', 'Swift', 'SystemVerilog', 'TypeScript', 'Verilog',
+        'Visual Basic (6 or earlier)', 'Visual Basic .NET', 'VBA', 'Zig',
+    )
+    libraries: tuple = (
+        '.NET 5+', '.NET Framework 1.0 - 4.8', '.NET MAUI', 'Angular',
+        'AngularJS', 'Apache Kafka', 'Apache Spark', 'ASP.Net', 'ASP.Net Core',
+        'bandit', 'Blazor', 'Boost.Test', 'Capacitor', 'Catch', 'CodeIgniter',
+        'Compose Multiplatform', 'Cordova', 'cppunit', 'CUDA', 'Deno',
+        'DirectX 12', 'DirectX 9 - 11', 'Django', 'doctest', 'Drupal',
+        'Electron', 'ELFspy', 'Elm', 'Express', 'FastAPI', 'Fastify', 'Flask',
+        'Flutter', 'Gatsby', 'GLFW', 'Google Test', 'GTK', 'Hadoop',
+        'Hugging Face Transformers', 'Ionic', 'JAX', 'jQuery', 'Keras',
+        'Kotlin Multiplatform', 'Kivy', 'Ktor', 'Laravel', 'liblittletest',
+        'Lit', 'Metal', 'MFC', 'Micronaut', 'NestJS', 'Next.js', 'Node.js',
+        'NumPy', 'Nuxt.js', 'OpenCL', 'OpenCV', 'OpenGL', 'Pandas', 'Phoenix',
+        'Play Framework', 'Qt', 'Quarkus', 'Qwik', 'RabbitMQ', 'React',
+        'React Native', 'Remix', 'Ruby on Rails', 'SciKit-Learn', 'SDL', 'SFML',
+        'snitch', 'Solid.js', 'Spring Boot', 'Spring Framework', 'Svelte',
+        'SwiftUI', 'Symfony', 'Tauri', 'TensorFlow', 'Tidyverse',
+        'Torch/PyTorch', 'Uno Platform', 'Vue.js', 'Vulkan', 'WordPress',
+        'Xamarin',
+    )
+    tools: tuple = (
+        'Ansible', 'Ant', 'APT', 'build2', 'Bun', 'Cargo', 'Chef', 'Chocolatey',
+        'CMake', 'Composer', 'Dagger', 'Docker', 'GNU GCC', 'Godot', 'Gradle',
+        'Homebrew', 'Kubernetes', 'LLVM Clang', 'Make', 'Maven', 'Meson',
+        'MSBuild', 'MSVC', 'Ninja', 'Nix', 'npm', 'NuGet', 'Pacman', 'Pip',
+        'pnpm', 'Podman', 'Pulumi', 'Puppet', 'QMake', 'Scons', 'Terraform',
+        'Unity 3D', 'Unreal Engine', 'Vite', 'Wasmer', 'Webpack', 'Yarn',
+    )
+    editors: tuple = (
+        'Android Studio', 'Atom', 'BBEdit', 'CLion', 'Code::Blocks', 'condo',
+        'DataGrip', 'Eclipse', 'Emacs', 'Fleet', 'Geany', 'Goland', 'Helix',
+        'IntelliJ IDEA', 'IPython', 'Jupyter Notebook/JupyterLab', 'Kate',
+        'Micro', 'Nano', 'Neovim', 'NetBeans', 'Notepad++', 'Nova', 'PhpStorm',
+        'PyCharm', 'Qt Creator', 'RAD Studio', 'Rider', 'RStudio', 'RubyMine',
+        'RustRover', 'Spyder', 'Sublime Text', 'TextMate', 'Vim',
+        'Visual Studio', 'Visual Studio Code', 'VSCodium', 'WebStorm', 'XCode',
+    )
+    databases: tuple = (
+        'BigQuery', 'Cassandra', 'Clickhouse', 'Cloud Firestore', 'CockroachDB',
+        'Cosmos DB', 'CouchDB', 'Datomic', 'DuckDB', 'DynamoDB',
+        'ElasticSearch', 'Firebase Realtime Database', 'Firebird', 'H2',
+        'IBM DB2', 'InfluxDB', 'MariaDB', 'Microsoft Access',
+        'Microsoft SQL Server', 'MongoDB', 'MySQL', 'Neo4J', 'Oracle',
+        'PostgreSQL', 'RavenDB', 'Redis', 'Snowflake', 'Solr', 'SQLite',
+        'Supabase', 'Text Files', 'TiDB',
+    )
+    cloud_platforms: tuple = (
+        'Amazon Web Services (AWS)', 'Cloudflare', 'Colocation', 'DigitalOcean',
+        'Firebase', 'Fly.io', 'Google Cloud Platform', 'Heroku', 'Hetzner',
+        'IBM Cloud or Watson', 'Linode (now Akamai)', 'Managed Hosting',
+        'Microsoft Azure', 'Netlify', 'OpenShift', 'OpenStack',
+        'Oracle Cloud Infrastructure', 'OVH', 'Render', 'Scaleway',
+        'Self-Hosted', 'Vercel', 'VMware', 'Vultr',
+    )
+    work_management_tools: tuple = (
+        'Adobe Workfront', 'Airtable', 'Asana', 'Azure DevOps', 'Basecamp',
+        'Cerri', 'ClickUp', 'Confluence', 'Dingtalk (Teambition)',
+        'Document360', 'Doxygen', 'GitHub Discussions', 'Jira', 'Leankor',
+        'Linear', 'Markdown File', 'Microsoft Lists', 'Microsoft Planner',
+        'Miro', 'Monday.com', 'Notion', 'Nuclino',
+        'Planview Projectplace or Clarizen', 'Redmine', 'Redocly', 'Shortcut',
+        'Smartsheet', 'StackOverflow for Teams', 'Swit', 'Tettra', 'Trello',
+        'Wikis', 'Workzone', 'Wrike', 'YouTrack',
+    )
+    communication_tools: tuple = (
+        'Cisco Webex Teams', 'Coolfire Core', 'Discord',
+        'Facebook Messenger', 'Google Chat', 'Google Meet', 'IRC', 'Jitsi',
+        'Matrix', 'Mattermost', 'Microsoft Teams', 'Ringcentral', 'Rocketchat',
+        'Signal', 'Skype', 'Slack', 'Symphony', 'Telegram', 'Unify Circuit',
+        'Whatsapp', 'Wickr', 'Wire', 'Zoom', 'Zulip',
+    )
+    ai_search_tools: tuple = (
+        'Andi', 'Bing AI', 'ChatGPT', 'Google Bard AI', 'Metaphor', 'Neeva AI',
+        'Perplexity AI', 'Phind', 'Quora Poe', 'WolframAlpha', 'You.com',
+        'Others',
+    )
+    ai_developer_tools: tuple = (
+        'Adrenaline', 'AWS CodeWhisperer', 'Codeium', 'GitHub Copilot',
+        'Mintlify', 'Replit Ghostwriter', 'Rubber Duck.AI', 'Synk Code',
+        'TabNine', 'Whispr AI', 'Others',
+    )
+
     for row in file:
         participant_id: int = _add_participant(
             db,
@@ -27,6 +125,47 @@ def convert(file: csv.DictReader, db: sqlite3.Cursor):
             row['Event Location'],
             row['Message for Dev8']
         )
+
+        if row['Educational Attainment'] in attainments_for_higher_acads:
+            _add_higher_academics(db,
+                                  participant_id,
+                                  row['College/University Alma Mater'],
+                                  row['Degree Programme'],
+                                  row['Academic Learnings'])
+
+        computer_used: str = row['Computer Used Most of the Time']
+        if computer_used in ('Desktop PC', 'Laptop PC',):
+            os: str = row['Operating System (PC)']
+            specs: list[str] = row['Computer Specs'].split(';')
+            _add_pc(db, participant_id, os, specs)
+        elif computer_used == 'Mac':
+            os: str = row['Operating System (Mac)']
+            model: str = row['Mac Model']
+            _add_mac(db, participant_id, model, os)
+        elif computer_used in ('Tablet', 'Smartphone',):
+            model: str = row['Tablet/Smartphone Model']
+            _add_mobile(db, participant_id, model)
+
+        if row['Occupation'] == 'Professional/Freelancer':
+            employer_location: str = row['Employer Location']
+            employment_arrangement: str = row['Employment Arrangement']
+            annual_income_range: str = row['Annual Income Range']
+            annual_income_satisfaction: str = row['Annual Income Satisfaction']
+            _add_employment(db,
+                            participant_id,
+                            employer_location,
+                            employment_arrangement,
+                            annual_income_range,
+                            annual_income_satisfaction)
+        elif row['Occupation'] == 'Student':
+            academic_arrangement: str = row['Academic Arrangement']
+            _add_academics(db, participant_id, academic_arrangement)
+
+        developer_activities: list[str] = row['Developer Activities'].split(';')
+        for activity in developer_activities:
+            _add_developer_activity(db, participant_id, activity)
+
+
 
 
 def _add_participant(db: sqlite3.Cursor,
